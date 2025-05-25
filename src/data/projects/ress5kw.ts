2a9e1f262159f37820cdc6463794f29011cd7b7f
Nicolas Bamamou <bamamounicolas@gmail.com>
2025-05-25 15:03:09 +0800

    added one battery and made the schedule call button working

--- src/data/projects/ress5kw.ts
+++ src/data/projects/ress5kw.ts
@@ -1,24 +1,23 @@
-import { Battery } from 'lucide-react';
+import { Battery, BatteryChargingIcon, FileText, FileSpreadsheet } from 'lucide-react';
 import type { Project } from './types';
 
 export const ress5kw: Project = {
   id: 10,
   title: 'Residential Battery Energy Storage (RBESS)',
-  category: 'battery',  image: './images/BESS/BESS 4.png',
+  category: 'battery',  image: './images/BESS/Battery 5kW module.png',
   images: [
-    '../images/BESS/BESS 1.png',
-    '../images/BESS/BESS 2.png',
-    '../images/BESS/BESS 3.png',  
-    '../images/BESS/BESS 4.png',
+    '../images/BESS/Battery 5kW module.png',
+    '../images/BESS/5kW module.png',
+    '../images/BESS/5kW ESS .png',  
   ],
   description: '5kW residential battery system designed for optimal energy management with solar integration.',
   fullDescription: 'Developed an innovative residential energy storage solution that seamlessly integrates with solar power systems. The project focused on maximizing energy efficiency and providing reliable backup power while maintaining user-friendly operation.',
   tags: ['Battery Energy Storage System', 'Renewable Energy', ' IoT/Smart Home', 'Energy Management', 'Battery Pack Design', 'BMS', 'Smart Metering'],
   icon: Battery,
-  startDate: 'September 2023',
-  duration: '6 months',
-  scope: 'Full System Design and Implementation of a 15kW',
-  role: 'Battery Energy Systems Engineer',
+  startDate: 'May 2023',
+  duration: '2 months',
+  scope: 'Full System Design and Implementation of a 5.2kW',
+  role: 'RBESS Engineer',
   responsibilities: [
     'Conducted feasibility studies for residential energy storage',
     'Analyzed energy consumption patterns',
@@ -38,7 +37,20 @@ export const ress5kw: Project = {
     'Implemented remote monitoring and reporting',
     'Ensured compliance with safety standards',
     'Enabled real-time energy usage analytics',
-    'Successfully deployed in 5 residential installations'  
+    'Successfully deployed in 15 residential installations'  
   ],
-   technologies: ['Autodesk Fusion 360 / SolidWorks', 'CAD/technical-Drawing', 'Battery Management System (BMS)', 'Fiber laser welding', 'Fast-Bidirectional Charging', 'Battery Pack Design', 'Energy Management System (EMS) / Smart Metering', 'IoT Sensors and Smart Home Integration', 'User Interface Design', 'Renewable Energy Integration and Grid Connection'],
+    links: [
+  {
+    title: 'Documentation',
+    url: '../projects/BESS/ecomotech5kw_battery_docs.html',
+    icon: FileText
+  },
+  {
+    title: 'CAD Files',
+    url: 'https://grabcad.com/library/14s2p-bess-1',
+    icon: FileSpreadsheet
+  },
+],
+
+  technologies: ['Autodesk Fusion 360 / SolidWorks', 'CAD/technical-Drawing', 'Battery Management System (BMS)', 'Fiber laser welding', 'Fast-Bidirectional Charging', 'Battery Pack Design', 'Energy Management System (EMS) / Smart Metering', 'IoT Sensors and Smart Home Integration', 'User Interface Design', 'Renewable Energy Integration and Grid Connection'],
 };
